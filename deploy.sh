7b8d3a3bf6a00daf1eba4051940f9ef5eec15aad
Yohaan Vakil <yvakil@redhat.com>
2021-08-16 13:08:08 +0530

    dockerfile for frontend

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 # [work in progress]
 
-oc login -u xfmitf -p 4da8cae9204a4abdb1f3
+# oc login -u xfmitf -p 4da8cae9204a4abdb1f3
 
-oc new-project yvakil-auto
+# oc new-project yvakil-auto
 
-oc new-app openshift/nodejs-010-centos7~https://github.com/yozaam/todo/ --context-dir=/frontend 
+oc new-app openshift/nodejs~https://github.com/yozaam/todo/#review --context-dir=/frontend 
 
-oc expose service/todo
+oc expose service/todo --port=3000
+# oc port-forward `oc get pods | awk 'END{print $1}'` 3000:8080 &
 
-oc port-forward `oc get pods | awk 'END{print $1}'` 3000:8080 &
