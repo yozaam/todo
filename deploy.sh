0eb4a6844526c03ff1320cc2c8c6974f290cb743
Yohaan Vakil <yvakil@redhat.com>
2021-08-16 15:58:48 +0530

    just specify openshift default PORT in package.json ?!

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@
 
 # oc new-project yvakil-auto
 
-oc new-app https://github.com/yozaam/todo/#review --context-dir=/frontend --strategy=docker
+oc new-app openshift/nodejs~https://github.com/yozaam/todo/#review --context-dir=/frontend 
+# put this for dockerfile --strategy=docker
 
 oc expose service/todo --port=3000
 # oc port-forward `oc get pods | awk 'END{print $1}'` 3000:8080 &
